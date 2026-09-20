e5e6fe9bd0f58306c27da2dcea35505a240173bd
MohamadHaris764 <Mohamad.37683@outlook.com>
2026-09-20 23:53:23 +0530

    Add files via upload

---
+++ 100-days-of-code-c.-/q3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a=5;
+int b=10;
+int area=a*b;
+int perimeter=2*(a+b);
+printf("value of area=%d" ,area);
+printf("value of perimeter=%d", perimeter);
+printf("value of %d and %d",a ,b);
+return 0;
+}
